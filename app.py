f7a8251cbc659542a4638a8ea3c06c5b27bc3618
ceradrake <137837432+ceradrake@users.noreply.github.com>
2024-04-14 08:51:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,42 @@
+import plotly.express as px
+from shiny.express import input, ui
+from shiny import reactive, render
+from shinywidgets import render_plotly
+import seaborn as sns
+import pandas as pd 
+from shinyswatch import theme 
 
+#Load the dataset
+titanic_df = sns.load_dataset("titanic")
+
+#Title 
+ui.page_opts(title="Cera's Titanic Dashboard", fillable=True)
+
+#Add a theme 
+theme.superhero()
+
+#Reactive Calc
+@reactive.calc
+def filtered_data():
+    survival = input.alive()
+    sex = input.sex()
+    selected_class = input.selected_class()
+    
+    filtered_df = titanic_df[
+        (titanic_df['alive'] == alive) &
+        (titanic_df['sex'] == sex) &
+        (titanic_df['class'] == selected_class)
+    ]
+    
+    return filtered_df
+
+#Create the sidebar
+with ui.sidebar(open="open"):
+    with ui.accordion():
+        with ui.accordion_panel("Survival"):
+            ui.input_selectize("alive", "Did They Survive?", 
+                               list(titanic_df['alive'].unique()))
+        with ui.accordion_panel("Gender"):
+            ui.input_selectize("sex", "Sex of the Passenger", list(titanic_df["sex"].unique()))
+        with ui.accordion_panel("Class"):
+            ui.input_selectize("class", "Class of the Passenger", list(titanic_df['class'].unique()))
